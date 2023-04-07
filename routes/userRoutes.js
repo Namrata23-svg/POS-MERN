5ad8bd40fd8215aa8d82fd80c3f2ef9c9fa4df28
Namrata23-svg <69518605+Namrata23-svg@users.noreply.github.com>
2023-04-07 23:03:18 +0530

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { loginController,registerController} = require('../controllers/userControllers')
+
+
+const router = express.Router()
+
+//routes
+//Method - get
+router.post('/login' ,loginController)
+
+//Method - POST
+router.post('/register', registerController)
+
+
+module.exports = router
